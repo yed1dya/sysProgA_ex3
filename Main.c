4d4d36ea2c99be8b1ba6897cd58dffb3151bba98
Yedidya <32509671+yed1dya@users.noreply.github.com>
2024-06-24 14:00:20 +0300

    Update Main.c

--- Main.c
+++ Main.c
@@ -3,14 +3,14 @@
 #include <stdlib.h>
 #include "StrList.h"
 
-void clearInput(){
-   char c;
-   while((c=getchar())!='\n'&&c!=EOF);
-}
-
 char* getString(){
+    //printf("getting string:\n");
     char*s = (char*)malloc(sizeof(char));
-    s = 0;
+    if(s==NULL){
+        printf("char malloc fail at line 10\n");
+        return NULL;
+    }
+    *s = '\0';
     char c = getchar();
     int length = 0; 
     while(c!=' ' && c!='\n' && c!= EOF){
@@ -19,53 +19,65 @@ char* getString(){
             free(s);
             exit(EXIT_FAILURE);
         }
-        *(s+length) = c;
-        *(s+length) = 0;
+        s[length] = c;
+        s[length+1] = 0;
+        length++;
         c = getchar();
     }
+    //printf("got string: %s\n",s);
     return s;
 }
 
 int main(){
+    //printf("starting\n");
     StrList*list = StrList_alloc();
     if(list==NULL){
+        printf("failed list malloc\n");
         StrList_free(list);
         exit(EXIT_FAILURE);
     }
     int input = -1;
     while(input!=0){
-        clearInput();
+        //printf("a) input: %d, enter input:\n",input);
         scanf("%d",&input);
+        //printf("b) input: %d\n",input);
         if(input==1){
-            clearInput();
+            //printf("1) enter length:\n");
             int length = -1;
             scanf("%d",&length);
-            clearInput();
+            getchar();
+            //printf("1.1) enter strings:\n");
             for(int i=0; i<length; i++){
                 char*s = getString();
                 StrList_insertLast(list,s);
+                //printf("a) length: %zu\n",StrList_size(list));
             }
+            //printf("b) length: %zu\n",StrList_size(list));
+            //StrList_print(list);
         }
         if(input==2){
-            clearInput();
             int index = -1;
+            //printf("2) enter index:\n");
             scanf("%d",&index);
-            if(index>=0 && index < StrList_size(list)){
-                clearInput();
+            getchar();
+            if(index>=0 && index <= StrList_size(list)){
+                //printf("2.1) enter string:\n");
                 char*s = getString();
                 StrList_insertAt(list,s,index);
             }
         }
         if(input==3){
+            //printf("3)\n");
             StrList_print(list);
         }
         if(input==4){
-            printf("%d\n",StrList_printLen(list));
+            printf("%zu\n",StrList_size(list));
         }
         if(input==5){
-            clearInput();
             int index = -1;
+            //printf("5) enter index:\n");
             scanf("%d",&index);
+            getchar();
             if(index>=0 && index < StrList_size(list)){
                 StrList_printAt(list,index);
             }
@@ -74,38 +86,50 @@ int main(){
             printf("%d\n",StrList_printLen(list));
         }
         if(input==7){
-            clearInput();
+            getchar();
+            //printf("7) enter string:\n");
             char*s = getString();
+            //printf("counting: %s\n",s);
             printf("%d\n",StrList_count(list,s));
         }
         if(input==8){
-            clearInput();
+            getchar();
+            //printf("8) enter string:\n");
             char*s = getString();
+            //printf("removing %s\n",s);
             StrList_remove(list,s);
         }
         if(input==9){
-            clearInput();
             int index = -1;
+            //printf("9) enter index:\n");
             scanf("%d",&index);
+            getchar();
             StrList_removeAt(list,index);
         }
         if(input==10){
+            //printf("10) reversing\n");
             StrList_reverse(list);
         }
         if(input==11){
+            //printf("11) erasing\n");
+            StrList_print(list);
             while(StrList_size(list)>0){
                 StrList_removeAt(list,0);
+                //StrList_print(list);
             }
         }
         if(input==12){
+            //printf("12) sorting\n");
             StrList_sort(list);
         }
         if(input==13){
+            //printf("13) is sorted?\n");
             if(StrList_isSorted(list)==1){
                 printf("true\n");
             }else printf("false\n");
         }
     }
+    //printf("c) input: %d\n",input);
     StrList_free(list);
     return 0;
 }
